d16f9270606dd15892ab832f63f81f0c510bdfda
DELL <Sandhali Gupta>
2024-06-23 00:02:36 -0700

    type conversion

--- basics.js
+++ basics.js
@@ -1,2 +1,25 @@
-console.log("Hello");
-console.log("hii");
+// console.log("Hello");
+// console.log("hii");
+const accountId = 1244;
+let city = "Jaipur";
+var state = "Uttar Pradesh"
+village = "Maukhas"
+//We can not change  const keyword
+// accountId = 89
+city = "Meerut"
+state = "UP"
+village="Sisoli"
+console.log(accountId)
+
+console.log(city)
+
+console.log(state)
+
+console.log(village)
+console.table([accountId, city, state, village])
+let num = 2;
+console.log(typeof null )
+let value = 1
+value = Boolean(value)
+console.log(typeof value)
+console.log(value)
